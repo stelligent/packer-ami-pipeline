f6724a2ae6bf10f9df8952c76569593368075e76
Jim Rohrer <jrohrer1@gmail.com>
2018-08-08 15:08:37 -0400

    Debugging

--- buildscripts/generate_instance_config.sh
+++ buildscripts/generate_instance_config.sh
@@ -2,7 +2,11 @@
 # Generate test-instance-config.json from Packer outputs
 
 ARTIFACT_ID=$(cat manifest.json | jq '.builds[0].artifact_id')
-IFS=: read REGION AMI_ID <<< $(sed -e 's/^"//' -e 's/"$//' <<< $ARTIFACT_ID)
+echo $ARTIFACT_ID
+echo $(sed -e 's/^"//' -e 's/"$//' <<< $ARTIFACT_ID)
+IFS=":" read REGION AMI_ID <<< $(sed -e 's/^"//' -e 's/"$//' <<< $ARTIFACT_ID)
+echo "Region: ${REGION}"
+echo "AMI ID: ${AMI_ID}"
 DATE=$(date '+%s')
 aws ec2 create-key-pair --key-name test-instance-key-${DATE} --query "KeyMaterial" --output text > test-instance-key-${DATE}.pem
 echo "{\"AmiId\": \"${AMI_ID}\", \"KeyName\": \"test-instance-key-${DATE}\"}" >> test/test-instance-config.json
