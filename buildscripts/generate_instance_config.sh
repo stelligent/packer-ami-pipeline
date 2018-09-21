3acb46ed8401c8e32ba8b585acc8e5617e643c3f
Jim Rohrer <jrohrer1@gmail.com>
2018-09-20 20:43:51 -0500

    Fixed test-instance location

--- buildscripts/generate_instance_config.sh
+++ buildscripts/generate_instance_config.sh
@@ -6,4 +6,4 @@ ARTIFACT_ID=$(cat manifest.json | jq '.builds[0].artifact_id')
 IFS=":" read REGION AMI_ID <<< $(sed -e 's/^"//' -e 's/"$//' <<< $ARTIFACT_ID)
 DATE=$(date '+%s')
 aws ec2 create-key-pair --key-name test-instance-key-${DATE} --query "KeyMaterial" --output text > test-instance-key-${DATE}.pem
-echo "{\"Parameters\": {\"AmiId\": \"${AMI_ID}\", \"KeyName\": \"test-instance-key-${DATE}\"}}" >> test/test-instance-config.json
+echo "{\"Parameters\": {\"AmiId\": \"${AMI_ID}\", \"KeyName\": \"test-instance-key-${DATE}\"}}" >> cfn/test-instance-config.json
